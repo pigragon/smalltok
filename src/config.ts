8b6f801cac5a3308aea6506e9d9a05b867312e7c
pigragon <pigragon@pigragon.kr>
2026-03-30 12:52:04 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   title: "Small.tok",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
+  postPerIndex: 8,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
